e291a92b8f93e8fc73a4e3a9e5c6566c3cbb89d8
James Soo <jamesklsoo@gmail.com>
2017-08-02 00:26:47 +0800

    add and update migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801100749) do
+ActiveRecord::Schema.define(version: 20170801154942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string "uid"
+    t.string "token"
+    t.string "provider"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comments"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -34,4 +60,6 @@ ActiveRecord::Schema.define(version: 20170801100749) do
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "authentications", "users"
+  add_foreign_key "likes", "posts"
 end
